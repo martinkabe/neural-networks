5b9d75e2162f98a8bd9e6a0cec5a5092f1f38d76
Martin <martinkabe@seznam.cz>
2023-01-22 21:50:53 +0100

    Add error output layer to multilayer perceptron implementation

--- multilayer-perceptron/multilayer_perceptron_implementation.py
+++ multilayer-perceptron/multilayer_perceptron_implementation.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def sigmoid(sum):
+    return 1 / (1 + np.exp(-sum))
+
+def sigmoid_derivative(sigmoid):
+    return sigmoid * (1 - sigmoid)
+
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 outputs = np.array([[0], [1], [1], [0]])
 weights0 = np.array([
@@ -9,11 +15,6 @@ weights0 = np.array([
 weights1 = np.array([[-0.017], [-0.893], [0.148]])
 epochs = 100
 
-
-def sigmoid(sum):
-    return 1 / (1 + np.exp(-sum))
-
-
 input_layer = inputs
 
 sum_synapse0 = np.dot(input_layer, weights0)
@@ -22,7 +23,22 @@ hiden_layer = sigmoid(sum_synapse0)
 
 sum_synapse1 = np.dot(hiden_layer, weights1)
 output_layer = sigmoid(sum_synapse1)
-print(output_layer)
+
+error_output_layer = outputs - output_layer
+average = np.mean(abs(error_output_layer))
+
+## delta_output = error * sigmoid_derivative
+derivative_output = sigmoid_derivative(output_layer)
+# print(derivative_output)
+delta_output = error_output_layer * derivative_output
+
+## delta_hidden = sigmoid_derivative * weight * delta_output
+weights1_T = weights1.T
+delta_output_weights1T = delta_output.dot(weights1_T)
+# print(delta_output_weights1T)
+delta_hidden_layer = sigmoid_derivative(hiden_layer) * delta_output_weights1T
+print(delta_hidden_layer)
+
 
 for epoch in range(epochs):
     pass
